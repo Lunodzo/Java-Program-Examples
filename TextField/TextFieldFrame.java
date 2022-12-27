31e95b02e2b3bdcd98d63386c14dbc234e7312f6
Lunodzo Mwinuka <lunomwinuka@gmail.com>
2022-12-27 21:58:00 +0300

    New classes from the book

--- TextField/TextFieldFrame.java
+++ TextField/TextFieldFrame.java
@@ -1,8 +1,14 @@
+package TextField;
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-import javax.swing.*;
-
-public class TextFieldFrame extends JFrame {
+public class TextFieldFrame extends JFrame{
     private JTextField textField1; // text field with set size
     private JTextField textField2; // text field constructed with text
     private JTextField textField3; // text field with text and size
@@ -10,13 +16,12 @@ public class TextFieldFrame extends JFrame {
 
     public TextFieldFrame(){
         super( "Testing JTextField and JPasswordField" );
-        setLayout( new LayoutManager() ); // set frame layout
+        setLayout( new FlowLayout() ); // set frame layout
         // construct textfield with 10 columns
         textField1 = new JTextField( 10 );
         add( textField1 ); // add textField1 to JFrame
-        
         // construct textfield with default text
-        textField2 = new JTextField("Enter text here");
+        textField2 = new JTextField( "Enter text here" );
         add( textField2 ); // add textField2 to JFrame
 
         // construct textfield with default text and 21 columns
@@ -29,34 +34,39 @@ public class TextFieldFrame extends JFrame {
         // register event handlers
         TextFieldHandler handler = new TextFieldHandler();
         textField1.addActionListener( handler );
-        textField2.addActionListener( handler );
+        textField2.addActionListener( (ActionListener) handler );
         textField3.addActionListener( handler );
         passwordField.addActionListener( handler );
-    }// End of TextField constructor
+    }
 
-// private inner class for event handling
-private class TextFieldHandler implements ActionListener{
-        // process text field events
-        public void actionPerformed( ActionEvent event ){
+
+    public class TextFieldHandler implements ActionListener{
+
+        @Override
+        public void actionPerformed(ActionEvent event) {
+            // TODO Auto-generated method stub
+
             String string = ""; // declare string to display
             // user pressed Enter in JTextField textField1
             if ( event.getSource() == textField1 )
-                string = String.format( "textField1: %s",
-                event.getActionCommand() );
+            string = String.format( "textField1: %s",
+            event.getActionCommand() );
             // user pressed Enter in JTextField textField2
             else if ( event.getSource() == textField2 )
-                string = String.format( "textField2: %s",
-                event.getActionCommand() );
+            string = String.format( "textField2: %s",
+            event.getActionCommand() );
             // user pressed Enter in JTextField textField3
             else if ( event.getSource() == textField3 )
-                string = String.format( "textField3: %s",
-                event.getActionCommand() );
+            string = String.format( "textField3: %s",
+            event.getActionCommand() );
             // user pressed Enter in JTextField passwordField
             else if ( event.getSource() == passwordField )
-                string = String.format( "passwordField: %s",
-                event.getActionCommand() );
+            string = String.format( "passwordField: %s",
+            event.getActionCommand() );
             // display JTextField content
             JOptionPane.showMessageDialog( null, string );
-        } // end method actionPerformed
-    } // end private inner class TextFieldHandler
+            
+        }
+        
+    }
 }
